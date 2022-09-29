598a3f636e9ddc4e96f61aeabba4bed375f8340f
jeremij94 <jeremij94@gmail.com>
2022-09-29 14:09:39 -0500

    first commit Factory Pattern Exercise

---
+++ FactoryPatternExercise/Motorcyle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace FactoryPatternExercise
+{
+    public class Motorcyle : IVehicle 
+    {
+        public Motorcyle()
+        {
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Motorcycle is driving.");
+        }
+    }
+}
+
---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace FactoryPatternExercise
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int numberOfTires)
+        {
+
+            
+            if (numberOfTires == 2)
+            {
+                return new Motorcyle();
+            }
+
+            else if (numberOfTires == 4)
+            {
+                return new Car();
+            }
+
+            else
+            {
+                return new Car();
+            }
+        }
+        
+    }
+
+}
+
